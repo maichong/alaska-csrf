92050b73a7553c85b9b34957e05aa62394f2533f
Liang Xingchen <liang@maichong.it>
2016-04-16 13:24:03 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * @copyright Maichong Software Ltd. 2016 http://maichong.it
+ * @date 2016-04-16
+ * @author Liang <liang@maichong.it>
+ */
+
+'use strict';
+
+module.exports = function (options) {
+  return function (ctx, next) {
+    
+  };
+};
